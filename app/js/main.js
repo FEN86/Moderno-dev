19694ae40c8184f548258b1228a1a82173b1b750
FEN <fentisov86@gmail.com>
2020-03-25 20:01:52 +0200

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -20,7 +20,30 @@ $(function () {
     arrows: false,
     infinite: true,
     slidesToShow: 4,
-    slidesToScroll: 4
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 1150,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+    ]
   });
 
   // rage-slider
